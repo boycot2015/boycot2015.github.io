95d5bd1675a768817bffc8c202190153c5cf0ca6
zhangchunhui <boycot2017@163.com>
2025-11-06 12:00:14 +0800

    🐛 fix(Subway): 修复地铁时间逻辑错误。
    - 移除不必要的 `hours` 变量。
    - 修复 `lastTime` 和 `firstTime` 的计算逻辑。
    - 更新提示信息以反映正确的末班车时间。
    
    ✨ feat(Subway): 新增末班车和首班车时间判断。
    
    - 添加 `lastTime` 和 `firstTime` 变量以计算末班车和首班车时间。
    - 更新提示信息以反映末班车和首班车的状态。

--- src/scripts/Subway.ts
+++ src/scripts/Subway.ts
@@ -2,7 +2,6 @@ import { getSubway, getBeijingTime } from '@/utils/index';
 export default async function GET() {
     const data = await getSubway();
     const time = getBeijingTime('YYYY-MM-DD HH:mm');
-    let hours = new Date(time).getHours();
     let current = data.data.subways[Math.floor(Math.random() * (data.data.subways.length - 1))];
     let currentIndex = Math.floor(Math.random() * (current.subways.length - 1));
     const nextStations = current.subways.filter((el:any) => !current.destination.includes(el.ZDMZ));
@@ -10,10 +9,13 @@ export default async function GET() {
     let subways = {
         subways: current.subways,
         current,
+        time: current.subways.find((el:any) => el?.time)?.time || time,
         destination: current.destination[Math.floor(Math.random() * current.destination.length)],
         line: current.line,
         nextStation: current.subways[currentIndex],
     }
+    let lastTime = new Date(new Date(time).setHours(subways?.time.LAST_SUBWAY_TIME?.split(':')[0], subways?.time.LAST_SUBWAY_TIME?.split(':')[1], 0, 0)).getTime();
+    let firstTime = new Date(new Date(time).setHours(subways?.time.FIRST_SUBWAY_TIME?.split(':')[0], subways?.time.FIRST_SUBWAY_TIME?.split(':')[1], 0, 0)).getTime();
     // console.log(subways, 'current');
     let subway:any = document.querySelector('#subway');
     let timeDOM:any = document.querySelector('#bj-time');
@@ -22,9 +24,13 @@ export default async function GET() {
     let transferText = `，乘客可换乘${subways?.nextStation?.transfer?.join('、')}号线`
     const text = `${nextText}${subways?.nextStation?.transfer?.length ? transferText : ''}`;
     subway.content = `欢迎乘坐深圳地铁${subways?.line || '5'}号线，本次列车开往${subways?.destination || '黄贝岭'}方向；${text}，请要下车的乘客带好您的行李物品准备下车；文明出行，礼让为先！`;
-    if (hours > 23 && hours <= 24) {
+    // console.log(subways, 'subway');
+    
+    if (lastTime < new Date(time).getTime() - subways?.time.INTERVAL.substring(0, 1) * 60 * 1000) {
+      subway.content = `开往${subways?.destination || '黄贝岭'}方向的末班车即将开出，请乘客留意发车时间，合理安排出行`;
+    } else if (lastTime < new Date().getTime()) {
       subway.content = `开往${subways?.destination || '黄贝岭'}方向的末班车已经开出，请乘客留意发车时间，合理安排出行`;
-    } else if (hours < 6) {
+    } else if (firstTime < new Date().setHours(6, 0, 0, 0)) {
       subway.content = `开往${subways?.destination || '黄贝岭'}方向的列车还未开出，请乘客留意发车时间，合理安排出行`;
     };
     document.querySelector('#subway')?.addEventListener('click', (e:any) => {
